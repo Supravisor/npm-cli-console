3d0ef846d0c88237841052409c560a620491e31d
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-12-18 11:23:17 +1300

    add CUT button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 <script>
   function cut() {
     // Get the text field
-    var copyText = document.getElementById("myInput");
+    var copyText = document.getElementById("input");
 
     // Copy the text inside the text field
     navigator.clipboard.writeText(copyText.value);
